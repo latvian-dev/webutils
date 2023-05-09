86c9fc1f081a068229a0e8d01a934b84d4067496
LatvianModder <latvianmodder@gmail.com>
2023-05-09 03:37:53 +0300

    Updated Possible

--- src/main/java/dev/latvian/apps/webutils/data/Possible.java
+++ src/main/java/dev/latvian/apps/webutils/data/Possible.java
@@ -2,20 +2,17 @@ package dev.latvian.apps.webutils.data;
 
 import org.jetbrains.annotations.Nullable;
 
-public record Possible<T>(@Nullable Object value) {
+import java.util.function.Function;
+
+@SuppressWarnings("unchecked")
+public record Possible<T>(@Nullable T value) {
 	public static final Possible<?> ABSENT = new Possible<>(null);
 	public static final Possible<?> NULL = new Possible<>(null);
 
-	public Possible(@Nullable Object value) {
-		this.value = value;
-	}
-
-	@SuppressWarnings("unchecked")
 	public static <T> Possible<T> of(@Nullable T o) {
 		return o == null ? (Possible<T>) NULL : new Possible<>(o);
 	}
 
-	@SuppressWarnings("unchecked")
 	public static <T> Possible<T> absent() {
 		return (Possible<T>) ABSENT;
 	}
@@ -32,9 +29,16 @@ public record Possible<T>(@Nullable Object value) {
 		return this == ABSENT ? "EMPTY" : String.valueOf(this.value);
 	}
 
-	@Override
+	public <C> Possible<C> cast(Class<C> type) {
+		return (Possible<C>) this;
+	}
+
+	public <C> Possible<C> map(Function<T, C> function) {
+		return isAbsent() ? absent() : Possible.of(function.apply(value));
+	}
+
 	@Nullable
-	public Object value() {
-		return this.value;
+	public T get() {
+		return isAbsent() ? null : get();
 	}
 }
