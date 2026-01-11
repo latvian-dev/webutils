e35f644a3dae0452a8a2bb107bdcea28564eb7c4
LatvianModder <latvianmodder@gmail.com>
2026-01-11 18:38:37 +0200

    Encode + as %20

--- src/main/java/dev/latvian/apps/webutils/CodingUtils.java
+++ src/main/java/dev/latvian/apps/webutils/CodingUtils.java
@@ -66,7 +66,7 @@ public interface CodingUtils {
 
 	static String encodeURL(String string) {
 		try {
-			return URLEncoder.encode(string, StandardCharsets.UTF_8);
+			return URLEncoder.encode(string, StandardCharsets.UTF_8).replace("+", "%20");
 		} catch (Exception ex) {
 			return string;
 		}
